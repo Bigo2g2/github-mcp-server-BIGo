f9beb924737376596f02749da5205c0716015db1
Bigo2g2 <hardcoremusicgroup@gmail.com>
2024-12-06 11:10:25 -0500

    Add Standings to Dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Standings from './Standings';
+
+const Dashboard = () => {
+  return (
+    &lt;div>
+      {/* ... */}
+      &lt;Standings />
+    &lt;/div>
+  );  
+};
+
+export default Dashboard;
